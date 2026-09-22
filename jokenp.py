84d9a9d09a04de28cbcbcb633ae2bb96871f6c00
Evellyn <evellynmonteiro934@gmail.com>
2026-09-22 18:02:59 -0400

    alteracoes

--- jokenp.py
+++ jokenp.py
@@ -1,26 +1,35 @@
 from random import randint
 from time import sleep
-itens= ('Pedra', 'Papel', 'Tesoura')
-computador= randint(0, 2)
 
-print ('''Sua opcoes: 
-'[0] PEDRA' 
-'[1] PAPEL' 
-'[2] TESOURA''')
-jogador= int(input('Qual a sua jogada? '))
-print ('JO')
+opcoes = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+
+print('''Suas opções:
+[0] PEDRA
+[1] PAPEL
+[2] TESOURA''')
+
+jogador = int(input('Qual a sua jogada? '))
+
+print('JO')
 sleep(1)
 print('KEN')
 sleep(1)
 print('PO!!')
 sleep(1)
-print ('..' * 20)
-print('Voce jogou {}'.format(itens[jogador]))
-print('Computador jogou {}'.format(itens[computador]))
+
+print('-' * 30)
+
+print(f'Você jogou {opcoes[jogador]}')
+print(f'Computador jogou {opcoes[computador]}')
 
 if computador == jogador:
-    print ('EMPATE')
-elif (jogador == 1 and computador == 0) or (jogador == 2 and computador == 1) or (jogador == 0 and computador == 2):
+    print('EMPATE')
+
+elif (jogador == 1 and computador == 0) or \
+     (jogador == 2 and computador == 1) or \
+     (jogador == 0 and computador == 2):
     print('VOCÊ VENCEU')
+
 else:
-    print ('VOCE PERDEU')
+    print('VOCÊ PERDEU')
